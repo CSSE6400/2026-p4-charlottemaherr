78aa11bda640d2b2ff9dfc4b4589e816297274f6
charlottemaherr <charlotte.maher@student.uq.edu.au>
2026-03-31 11:35:52 +1000

    Formatting fixes to pass unit tests

--- main.tf
+++ main.tf
@@ -35,28 +35,28 @@ output "hextris-url" {
     value = "http://${aws_instance.hextris-server.public_ip}/"
 }
 
-resource "aws_security_group" "hextris-server" { 
- name = "hextris-server" 
- description = "Hextris HTTP and SSH access" 
- 
- ingress { 
-   from_port = 80 
-   to_port = 80 
-   protocol = "tcp" 
-   cidr_blocks = ["0.0.0.0/0"] 
- } 
- 
- ingress { 
-   from_port = 22 
-   to_port = 22 
-   protocol = "tcp" 
-   cidr_blocks = ["0.0.0.0/0"] 
- } 
- 
- egress { 
-   from_port = 0 
-   to_port = 0 
-   protocol = "-1" 
-   cidr_blocks = ["0.0.0.0/0"] 
- } 
+resource "aws_security_group" "hextris-server" {
+    name = "hextris-server"
+    description = "Hextris HTTP and SSH access"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
